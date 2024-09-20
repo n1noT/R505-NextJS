8b4f1a334feb94db94f17b8e2be5bd5103dfc764
Nino Toutain <nino.toutain@etu.unilim.fr>
2024-09-20 16:58:01 +0200

    nsnfsfspfsgdgdg

--- nextjs-teamproject/app/ui/NavBar/NavBarLinks.tsx
+++ nextjs-teamproject/app/ui/NavBar/NavBarLinks.tsx
@@ -2,7 +2,12 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
-export default function NavBarLinks  ({link, content}) {
+interface NavBarLinksProps {
+    link: string;
+    content: string;
+  }
+
+export default function NavBarLinks  ({link, content}: NavBarLinksProps) {
     const pathname = usePathname();
     return(
         <Link className={clsx("mb-4 lg:mb-0 lg:pe-2 text-[#BBA]", { 'bg-sky-100 text-blue-600': pathname === link})} href={link}>{content}</Link>
